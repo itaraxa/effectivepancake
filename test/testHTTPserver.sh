a3ff3328c2c1cf3c9c2e0293c37522ea214ceb09
Aleksey Shaforostov <itaraxa@yandex.ru>
2024-09-28 16:13:58 +0300

    Added script for funcional testing

---
+++ test/testHTTPserver.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+interval=1
+
+curls=(
+"curl -v -X POST 'http://localhost:8080'"
+"curl -v -X POST 'http://localhost:8080/update/gauge/test1/88.88' -H 'Content-Type: text/html' -d ''"
+"curl -v -X POST 'http://localhost:8080/update/gauge/test1/88.102' -H 'Content-Type: text/html' -d ''"
+"curl -v -X POST 'http://localhost:8080/update/gauge/test2/-32.102' -H 'Content-Type: text/html' -d ''"
+"curl -v -X POST 'http://localhost:8080/update/counter/test1/4' -H 'Content-Type: text/html' -d ''"
+"curl -v -X POST 'http://localhost:8080/update/counter/test1/5' -H 'Content-Type: text/html' -d ''"
+)
+
+for curl_cmd in "${curls[@]}"
+do
+  eval "$curl_cmd"
+  sleep $interval
+done
