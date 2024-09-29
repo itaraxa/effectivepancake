3528d30bae0c1477a59f32c261069c69c2051e57
Aleksey Shaforostov <itaraxa@yandex.ru>
2024-09-29 12:31:33 +0300

    Init commit in the development of the Agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+
+	"github.com/itaraxa/effectivepancake/internal/errors"
+)
+
+func main() {
+	body := []byte(``)
+
+	req, err := http.NewRequest("POST", "http://localhost:8080/update/counter/Agent1/123", bytes.NewBuffer(body))
+	if err != nil {
+		fmt.Printf("Error: %v", errors.ErrRequestCreating)
+		return
+	}
+
+	req.Header.Set("Content-Type", "txt/html")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Error: %v", errors.ErrRequestSending)
+		return
+	}
+
+	if resp.StatusCode == http.StatusOK {
+		fmt.Println("Status code = 200. Succes request")
+	} else {
+		fmt.Printf("Status code = %d", resp.StatusCode)
+	}
+
+	defer resp.Body.Close()
+
+}
--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -3,10 +3,15 @@ package errors
 import "errors"
 
 var (
+	// Server errors
 	ErrParseGauge   = errors.New("gauge value parsing error")
 	ErrParseCounter = errors.New("counter value parsing error")
 	ErrBadType      = errors.New("bad type in raw query string")
 	ErrBadName      = errors.New("bad name in raw query string")
 	ErrBadValue     = errors.New("bad value in raw query string")
 	ErrBadRawQuery  = errors.New("bad raw query string")
+
+	// Agent errors
+	ErrRequestCreating = errors.New("creating request error")
+	ErrRequestSending  = errors.New("sending request error")
 )
