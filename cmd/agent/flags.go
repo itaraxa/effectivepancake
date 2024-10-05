30baacb359e79e7f0721e3b79191d22412df2cf1
Aleksey Shaforostov <itaraxa@yandex.ru>
2024-10-05 12:10:13 +0300

    The logic for handling polling and reporting interval flags has been modified

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -17,8 +17,11 @@ func parseFlags() {
 	var showVersion bool
 	flag.BoolVar(&showVersion, `v`, false, `Show version and exit`)
 	flag.StringVar(&config.addressServer, `a`, `localhost:8080`, `HTTP-server endpoint address`)
-	flag.DurationVar(&config.pollInterval, `p`, 2*time.Second, `metrics poll interval`)
-	flag.DurationVar(&config.reportInterval, `r`, 10*time.Second, `metrics report interval`)
+	// flag.DurationVar(&config.pollInterval, `p`, 2*time.Second, `metrics poll interval`)
+	// flag.DurationVar(&config.reportInterval, `r`, 10*time.Second, `metrics report interval`)
+	var p, r int64
+	flag.Int64Var(&p, `p`, 2, `metrics poll interval, seconds`)
+	flag.Int64Var(&r, `r`, 10, `metrics report interval, seconds`)
 
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "Version: %s\nUsage of %s\n", version, os.Args[0])
@@ -30,6 +33,10 @@ func parseFlags() {
 		fmt.Fprintf(os.Stderr, "Error parsing flags: %v", err)
 		os.Exit(1)
 	}
+
+	config.pollInterval = time.Duration(p) * time.Second
+	config.reportInterval = time.Duration(r) * time.Second
+
 	if showVersion {
 		fmt.Printf("Agent version: %s\n\r", version)
 		os.Exit(0)
