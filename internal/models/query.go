1ee3e5806e73c74f513f4eda6e8cb3a7fb78579f
Aleksey Shaforostov <itaraxa@yandex.ru>
2024-10-05 21:38:22 +0300

    Added comments and unit tests to models.Query

--- internal/models/query.go
+++ internal/models/query.go
@@ -12,6 +12,21 @@ type Query struct {
 	MetricRawValue string
 }
 
+/*
+Get string representation instance of Query
+
+Returns:
+
+	string: string representation of the Query instance
+
+Output example:
+
+	==== Query ====
+	>Raw: update/gauge/test1/3.14
+	>>Type: gauge
+	>>Name: test1
+	>>Value: 3.14
+*/
 func (q Query) String() string {
 	s := "==== Query ====\n\r"
 	s += fmt.Sprintf(">Raw: %s\n\r", q.raw)
@@ -22,14 +37,35 @@ func (q Query) String() string {
 	return s
 }
 
+/*
+Get metrica type
+
+Returns:
+
+	string: Type of metrica
+*/
 func (q Query) GetMetricaType() string {
 	return q.MetricType
 }
 
+/*
+Get metrica name
+
+Returns:
+
+	string: Name of metrica
+*/
 func (q Query) GetMetricName() string {
 	return q.MetricName
 }
 
+/*
+Get metrica raw value (string representation of value)
+
+Returns:
+
+	string: string representation of value
+*/
 func (q Query) GetMetricaRawValue() string {
 	return q.MetricRawValue
 }
